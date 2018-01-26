ad69793753ce8b69f58c0c5d61bc396838f5d73e
gerbenvandervries <gerbenvandervries@gmail.com>
2018-01-26 12:41:25 +0100

    layout report

--- protocols/qcReport.sh
+++ protocols/qcReport.sh
@@ -13,40 +13,41 @@
 
 
 
-rm -f "${resultsImputed}"/QcReport.txt
+rm -f "${resultsImputed}"/QC_Report.txt
 
-echo "# Number of samples before and after sample removal." >> "${resultsImputed}"/QcReport.txt
+echo "## Number of samples before and after sample removal. ##" >> "${resultsImputed}"/QC_Report.txt
 
 for i in {0..21}
 do
-	echo "# chr$((${i}+1)):" >> "${resultsImputed}"/QcReport.txt
-	echo "Imputed dataset:"
-	grep 'Number of' ${jobsDirectory}/s2_removeSamples_${i}.out >> ${resultsImputed}/QcReport.txt
-	echo "" >> ${resultsImputed}/QcReport.txt
-	echo "Unimputed dataset:" >> ${resultsImputed}/QcReport.txt
-	grep -P "cluded" s3_removeSamplesUnimputed_${i}.out >> ${resultsImputed}/QcReport.txt
-	echo "" >> ${resultsImputed}/QcReport.txt
+	echo "" >> ${resultsImputed}/QC_Report.txt
+	echo "chr$((${i}+1)):" >> "${resultsImputed}"/QC_Report.txt
+	echo "Imputed dataset:" >> "${resultsImputed}"/QC_Report.txt
+	grep 'Number of' ${jobsDirectory}/s2_removeSamples_${i}.out >> ${resultsImputed}/QC_Report.txt
+	echo "" >> ${resultsImputed}/QC_Report.txt
+	echo "Unimputed dataset:" >> ${resultsImputed}/QC_Report.txt
+	grep -P "cluded" s3_removeSamplesUnimputed_${i}.out >> ${resultsImputed}/QC_Report.txt
 done
 
-echo "" >> ${resultsImputed}/QcReport.txt
-echo "# Number of collumns before and after samples removal." >> ${resultsImputed}/QcReport.txt
+echo "" >> ${resultsImputed}/QC_Report.txt
+echo "## Number of collumns before and after samples removal. ##" >> ${resultsImputed}/QC_Report.txt
+echo "" >> ${resultsImputed}/QC_Report.txt
 
 for i in "${chr[@]}"
 do
-	echo "chr${i}" >> ${resultsImputed}/QcReport.txt
-	echo "Inputed dataset:" >> ${resultsImputed}/QcReport.txt
-	cat "${concordanceDirectory}/chr${i}.gen.count.imputed.before" >> "${resultsImputed}"/QcReport.txt
-	cat "${concordanceDirectory}/chr${i}.gen.count.imputed.after" >> "${resultsImputed}"/QcReport.txt
-	echo "Uninputed dataset:" >> ${resultsImputed}/QcReport.txt
-	cat "${concordanceDirectory}/chr${i}.haps.count.unimputed.before" >> "${resultsImputed}"/QcReport.txt
-        cat "${concordanceDirectory}/chr${i}.haps.count.unimputed.after" >> "${resultsImputed}"/QcReport.txt
-	echo "lowest sample concordance unimputed dataset:" >> "${resultsImputed}"/QcReport.txt
-	awk '{print $5}' "${concordanceDirectory}/concordance_unimputed_chr${i}.sample" | sort | head -2 >> "${resultsImputed}"/QcReport.txt
-	echo "lowest sample concordance imputed dataset:" >> "${resultsImputed}"/QcReport.txt
-        awk '{print $5}' "${concordanceDirectory}/concordance_imputed_chr${i}.sample" | sort | head -2 >> "${resultsImputed}"/QcReport.txt
-	echo "" >> "${resultsImputed}"/QcReport.txt
+	echo "Inputed dataset:" >> ${resultsImputed}/QC_Report.txt
+	cat "${concordanceDirectory}/chr${i}.gen.count.imputed.before" >> "${resultsImputed}"/QC_Report.txt
+	cat "${concordanceDirectory}/chr${i}.gen.count.imputed.after" >> "${resultsImputed}"/QC_Report.txt
+	echo "Uninputed dataset:" >> ${resultsImputed}/QC_Report.txt
+	cat "${concordanceDirectory}/chr${i}.haps.count.unimputed.before" >> "${resultsImputed}"/QC_Report.txt
+        cat "${concordanceDirectory}/chr${i}.haps.count.unimputed.after" >> "${resultsImputed}"/QC_Report.txt
+	echo "" >> ${resultsImputed}/QC_Report.txt
+	echo "lowest sample concordance unimputed dataset:" >> "${resultsImputed}"/QC_Report.txt
+	awk '{print $5}' "${concordanceDirectory}/concordance_unimputed_chr${i}.sample" | sort | head -2 >> "${resultsImputed}"/QC_Report.txt
+	echo "lowest sample concordance imputed dataset:" >> "${resultsImputed}"/QC_Report.txt
+        awk '{print $5}' "${concordanceDirectory}/concordance_imputed_chr${i}.sample" | sort | head -2 >> "${resultsImputed}"/QC_Report.txt
+	echo "" >> "${resultsImputed}"/QC_Report.txt
 done
 
 cd "${resultsImputed}"
-cp ${resultsImputed}/QcReport.txt ../
+cp ${resultsImputed}/QC_Report.txt ../
 cd -
